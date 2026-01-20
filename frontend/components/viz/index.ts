f2543ba249a4646ea1f9b2f2e30462c59aa285a7
AriyanAzami <ariyan.azami84@gmail.com>
2026-01-19 19:49:57 -0500

    [UX]

---
+++ frontend/components/viz/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Visualization Components Index
+ * 
+ * PURPOSE:
+ * Central export point for all visualization components in the viz folder.
+ * Makes imports cleaner: `import { DealGradeGauge } from '@/components/viz'`
+ * instead of: `import { DealGradeGauge } from '@/components/viz/DealGradeGauge'`
+ * 
+ * EXPORTS:
+ * - DealGradeGauge: Speedometer visualization for deal grades (S/A/B/C/F)
+ * - DealGradeGaugeDemo: Demo component showing all grades
+ * 
+ * Last updated: 2026-01-19
+ */
+
+export { DealGradeGauge } from './DealGradeGauge';
+export { DealGradeGaugeDemo } from './DealGradeGaugeDemo';
+
+// Future exports will go here as we build more viz components:
+// export { PriceComparisonBar } from './PriceComparisonBar';
+// export { TCOChart } from './TCOChart';
